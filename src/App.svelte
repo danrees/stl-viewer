7f4f3790350843ab7166ad5328560bcde2686f44
Daniel Rees <daniel_james_rees@hotmail.com>
2023-04-19 20:37:01 -0400

    fix: make tabs active by page

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,29 @@
   import "./styles.css";
   import Router from "svelte-spa-router";
   import { routes } from "./routes";
+  import { location } from "svelte-spa-router";
 </script>
 
 <main class="container p-5">
   <div class="tabs">
-    <a href="#/" class="tab tab-bordered">Home</a>
-    <a href="#/library" class="tab tab-bordered">Libraries</a>
-    <a href="#/files" class="tab tab-bordered">Files</a>
-    <a href="#/tags" class="tab tab-bordered">Tags</a>
+    <a href="#/" class="tab tab-bordered" class:tab-active={$location == "/"}
+      >Home</a
+    >
+    <a
+      href="#/library"
+      class="tab tab-bordered"
+      class:tab-active={$location.includes("library")}>Libraries</a
+    >
+    <a
+      href="#/files"
+      class="tab tab-bordered"
+      class:tab-active={$location.includes("files")}>Files</a
+    >
+    <a
+      href="#/tags"
+      class="tab tab-bordered"
+      class:tab-active={$location.includes("tags")}>Tags</a
+    >
   </div>
   <Router {routes} />
 </main>
